87eacdd4b9399bff5e84961af13a7e4b759f060b
Julian Montagna <julian.montagna@tooit.com>
2015-10-27 02:54:02 -0300

    added support for endpoint metadata

--- client/main.js
+++ client/main.js
@@ -5,12 +5,22 @@ Router.route('/', function () {
 });
 
 BooksCT = new ContentType({
-  collection:   Books, // The collection defined above.
-  ctid:         "books", // The unique identifier for this content type.
-  title:        "Books", // Human redable name.
+  collection:       Books, // The collection defined above.
+  ctid:             "book", // The unique identifier for this content type.
+  title:            "Book", // Human redable name.
   meta: {
-    title:      "Books",
-    summary:    "A book is a set of written, printed, illustrated, or blank sheets, made of ink, paper, parchment, or other materials, fastened together to hinge at one side. See https://en.wikipedia.org/wiki/Book",
-    help:       "A book could not have a summary but it must have a title."
+    title:          "Book",
+    summary:        "A book is a set of written, printed, illustrated, or blank sheets, made of ink, paper, parchment, or other materials, fastened together to hinge at one side. See https://en.wikipedia.org/wiki/Book",
+    help:           "A book could not have a summary but it must have a title."
+  },
+  endpoints: {
+    create: {
+      enabled:      true,
+      meta: {
+        title:      "Create a new Book",
+        summary:    "Complete this form to add a new Book.",
+        help:       "Title, Author and Numbers of copies are required."
+      }
+    }
   }
 });
